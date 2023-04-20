73140a11623841cba51430b554e406b12d61c94d
Isaac <isaac.lin.pnc@gmail.com>
2023-04-20 11:26:23 -0400

    finished lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,13 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = None
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        result = node
+      else:
+        nodes_to_visit = nodes_to_visit + node['children']
+    return result
